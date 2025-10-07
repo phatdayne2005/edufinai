1a4f7ed018faa52a99349eff2daf3ebddeb513fe
PhatDepZai <thanhphat040@gmail.com>
2025-10-07 23:32:29 +0700

    Initial Microservice Project

---
+++ gamification-service/src/test/java/vn/uth/gamificationservice/GamificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.uth.gamificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
